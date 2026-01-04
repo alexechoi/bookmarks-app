a2154b1f9195b7aa9de1ab9cbf2d497bff7e0bf5
alexchvod <uceiac8@ucl.ac.uk>
2026-01-04 18:19:55 +0000

    Remove unused Android icon images from app.config.ts and delete corresponding image files to streamline asset management.

--- expo-app/app.config.ts
+++ expo-app/app.config.ts
@@ -44,9 +44,6 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   android: {
     adaptiveIcon: {
       backgroundColor: "#E6F4FE",
-      foregroundImage: "./assets/images/android-icon-foreground.png",
-      backgroundImage: "./assets/images/android-icon-background.png",
-      monochromeImage: "./assets/images/android-icon-monochrome.png",
     },
     edgeToEdgeEnabled: true,
     package: "xyz.alexchoi.bookmarksapp",
